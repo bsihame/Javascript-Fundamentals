c521076b46a90c82b2c8fff5c3cd35f84d4988d9
bsihame <sihamebazi@pursuit.org>
2021-02-03 18:49:19 -0500

    review  types  operator

---
+++ ValuesTypesOperators/ValuesTypesOperators.js
@@ -0,0 +1,133 @@
+// Problem One
+// What are the types of the following expressions and what do they evaluate to?
+
+17
+// 17
+
+1 + 2 * 3 + 4
+// 11
+
+800 / 80 / 8
+// 1.25
+
+400 > 200
+// true
+
+1 !== 1
+// false
+
+true || false
+// true
+
+true && false
+// false
+
+20 % 6
+// 2
+
+"a" + "b"
+// ab
+// =========================================
+// Problem Two
+// What will the following return?
+
+typeof 4
+// number
+
+typeof "hello"
+// string
+
+typeof true
+// boolean
+
+2 === 1 || 3 === 4
+// false
+// ============================================
+// Problem Three
+// Create a truth table for the expression A || B.
+
+// For reference, here is a truth table for the expression A && B. You can fill out the last column. Don't worry about keeping the spacing exact.
+
+A	B	A && B	A ll B
+true	true	true	
+false	true	false	
+true	false	false	
+false	false	false	
+  
+|  A    	|  B    	|  A && B  	|  A \|\| B  	|
+|-------	|-------	|----------	|------------	|
+|  true 	|  true 	|   true   	|    true    	|
+| false 	|  true 	|   false  	|    true    	|
+|  true 	| false 	|   false  	|    true    	|
+| false 	| false 	|   false  	|    false   	||
+
+  
+// Problem Four
+// Create a truth table for the expression !A && !B.
+
+// For reference, here is a truth table for the expression A && !B. You can fill out the last column. Don't worry about keeping the spacing exact.
+
+A	B	!B	A && !B	!A && !B
+true	true	false	false	
+false	true	false	false	
+true	false	true	true	
+false	false	true	false	
+
+|  A    	|  B    	| !B    	|  A && !B  	|  !A \|\| !B  	|
+|-------	|-------	|-------	|-----------	|--------------	|
+|  true 	|  true 	| false 	|   false   	|     false    	|
+| false 	|  true 	| false 	|   false   	|     true     	|
+|  true 	| false 	| true  	|    true   	|     true     	|
+| false 	| false 	| true  	|   false   	|     true     	|
+
+  //====================================
+  // Problem Five
+
+|   A   	|   B   	|  A \|\| B 	| !(A \|\| B) 	|
+|:-----:	|:-----:	|:---------:	|:-----------:	|
+|  true 	|  true 	|    true   	|    false    	|
+| false 	|  true 	|    true   	|    false    	|
+|  true 	| false 	|    true   	|    false    	|
+| false 	| false 	|   false   	|     true    	| 
+
+// Problem Six
+// Write a step-by-step evaluation for the following expression (remember order of operations): 2 + 3 * 2 + 1. For reference, here is a exp of a step-by-step evaluation:
+2 + 3 * 2 + 1.
+2 + 6 + 1.
+8 + 1
+9
+
+// Problem Seven
+// Write a step-by-step evaluation for the following expression (remember order of operations): 4 / 2 + 8 / 4
+
+4 / 2 + 8 / 4
+2 + 8 / 4
+2 + 2
+4.
+
+// Problem Eight
+// Write a step-by-step evaluation for the following expression: 'ca' + 'ter' + 'pi' + 'llar'.
+
+'ca' + 'ter' + 'pi' + 'llar'.
+'cater' + 'pi' + 'llar'
+'caterpi' + 'llar'
+'caterpillar'.
+
+// Problem Nine
+// Write a step-by-step evaluation for the following expression: 2 * 4 === 8 && 'car' + 'pool' === 'carpool'.
+
+2 * 4 === 8 && 'car' + 'pool' === 'carpool'.
+    8 === 8 && 'car' + 'pool' === 'carpool'.
+      true && 'carpool' === 'carpool'.
+        true && true
+true
+          
+// Problem Ten
+// Write a step-by-step evaluation for the following expression: '1' + '2' + '3' - '1'.
+
+'1' + '2' + '3' - '1'.
+'12' + '3' - '1'.
+
+'123' - '1'
+122
+
---
+++ ValuesTypesOperators/ValuesTypesOperators.md
@@ -0,0 +1,99 @@
+Values, Types & Operators
+Practice reading JS code and understanding what it does!
+
+Learning Objectives
+Understand different types in javascript
+Practice reading code & understanding boolean logic
+Learn the various operators in javascript and what they do
+Prerequisites
+Forking & cloning a repo from git
+Making a pull request
+Getting Started
+Fork this repository
+Clone the forked repository to your computer
+cd to the cloned directory
+code . to open the repo in VSCode
+When you are done, commit and push your work to your forked copy
+Make a pull request on github against the original repo
+Instructions
+For problems 1 and 2, write your answer in a comment (comments start with //) immediately below the line of code.
+
+Example:
+
+2 + 2
+// number, 4
+For problems 3-5, fill out the existing table or create a new one. See the table syntax guide
+
+For problems 6-10, see the example in problem 6 and write your own answer below each question.
+
+Problem One
+What are the types of the following expressions and what do they evaluate to?
+
+17
+
+1 + 2 * 3 + 4
+
+800 / 80 / 8
+
+400 > 200
+
+1 !== 1
+
+true || false
+
+true && false
+
+20 % 6
+
+"a" + "b"
+Problem Two
+What will the following return?
+
+typeof 4
+
+typeof "hello"
+
+typeof true
+
+2 === 1 || 3 === 4
+Problem Three
+Create a truth table for the expression A || B.
+
+For reference, here is a truth table for the expression A && B. You can fill out the last column. Don't worry about keeping the spacing exact.
+
+A	B	A && B	A ll B
+true	true	true	
+false	true	false	
+true	false	false	
+false	false	false	
+Problem Four
+Create a truth table for the expression !A && !B.
+
+For reference, here is a truth table for the expression A && !B. You can fill out the last column. Don't worry about keeping the spacing exact.
+
+A	B	!B	A && !B	!A && !B
+true	true	false	false	
+false	true	false	false	
+true	false	true	true	
+false	false	true	false	
+Problem Five
+Create a truth table for the expression !(A || B).
+
+Problem Six
+Write a step-by-step evaluation for the following expression (remember order of operations): 2 + 3 * 2 + 1. For reference, here is a exp of a step-by-step evaluation:
+
+1 + 2 + 3 + 4
+3 + 3 + 4
+6 + 4
+10
+Problem Seven
+Write a step-by-step evaluation for the following expression (remember order of operations): 4 / 2 + 8 / 4.
+
+Problem Eight
+Write a step-by-step evaluation for the following expression: 'ca' + 'ter' + 'pi' + 'llar'.
+
+Problem Nine
+Write a step-by-step evaluation for the following expression: 2 * 4 === 8 && 'car' + 'pool' === 'carpool'.
+
+Problem Ten
+Write a step-by-step evaluation for the following expression: '1' + '2' + '3' - '1'.
