80482aee17b5997ca69227c454ca857d19452323
bsihame <sihamebazi@pursuit.org>
2021-02-20 10:23:11 -0500

    split and  join method

---
+++ Array/array.js
@@ -0,0 +1,21 @@
+let fruits = ["Blueberry", "Banana",
+"orange", "kiwi", "Cherry"];
+console.log(fruits); // [ 'Blueberry', 'Banana', 'orange', 'kiwi', 'Cherry' ]
+// length property
+console.log(fruits.length); // ==>5
+// index start at 0
+console.log(fruits[0]);//==>Blueberry
+console.log(fruits[fruits.length-1]); // Cherry
+// I can reassign the specific array
+console.log(fruits[0]="apple");
+console.log(fruits);
+let num = [13, 14, 15, 16];// [ 15 ]
+console.log(num.slice(2, 3));//[ 15, 16 ]
+console.log(num.slice(2));
+console.log(num.slice(0,2));//[ 13, 14 ]
+console.log(num.slice(2,2));//[]
+let string = "My dog has fleas";
+console.log(string.split(" ")); // [ 'My', 'dog', 'has', 'fleas' ] // split change the string to array 
+console.log(fruits.join(" "));// apple Banana orange kiwi Cherry
+console.log(typeof(fruits.join(" "))); //join change the array to sting
+
---
+++ Array/index.md
@@ -0,0 +1,11 @@
+what are array
+Properties of length
+index
+modify
+method push shift unshift pop
+slice
+join
+
+What is array?
+Array is collection of stuff inside square bracket and separate by comma
+array are mutable( changeable) when we change it we cannot get it back. ( make sure to make a copy if you wan use your array again)
